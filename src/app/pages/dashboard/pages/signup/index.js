f5bb2f9cdb8a402cb576af7d76b8af037819ce36
lfabl <met2net3@gmail.com>
2021-03-21 13:28:38 +0300

    * Signup completed.

--- src/app/pages/dashboard/pages/signup/index.js
+++ src/app/pages/dashboard/pages/signup/index.js
@@ -18,13 +18,18 @@ import {
 import {
     gql
 } from '@apollo/client';
+import md5 from 'md5';
+
+const USER_NAME_REGEX = /^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
 
 const Signup = ({
-    classes
+    classes,
+    history
 }) => {
     const [fullName, setFullName] = useState("");
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
+    const [rePassword, setRePassword] = useState("");
 
     const [tokens, setTokens] = useTokens();
     const [theme, setTheme] = useTheme();
@@ -39,6 +44,51 @@ const Signup = ({
     } = tokens;
 
     const register = () => {
+        const _fullName = JSON.parse(JSON.stringify(fullName));
+        const _userName = JSON.parse(JSON.stringify(userName));
+        const _password = JSON.parse(JSON.stringify(password));
+        const _rePassword = JSON.parse(JSON.stringify(rePassword));
+
+        if(_password.trim() === "") {
+            alert("Parola boş olamaz.");
+            return false;
+        }
+
+        if(_userName.trim() === "") {
+            alert("Kullanıcı adı boş olamaz.");
+            return false;
+        }
+
+        if(_fullName.trim() === "") {
+            alert("Tam ad boş olamaz.");
+            return false;
+        }
+
+        if(_password.trim() !== _rePassword.trim()) {
+            alert("Lütfen parola tekrarı ile parolayı eşit giriniz.");
+            return false;
+        }
+
+        if(_password.trim().length > 80 || _password.trim().length < 8) {
+            alert("Parola uzunluğu 8 ile 80 arasında olmalıdır.");
+            return false;
+        }
+
+        if(_userName.trim().length > 50 || _userName.trim().length < 8) {
+            alert("Kullanıcı adı uzunluğu 8 ile 50 arasında olmalıdır.");
+            return false;
+        }
+
+        if(!USER_NAME_REGEX.test(_userName.trim())) {
+            alert("Kullanıcı adı bir kullanıcı adı olma şartlarını taşımıyor.");
+            return false;
+        }
+
+        if(_fullName.trim().length > 100 || _fullName.trim().length < 4) {
+            alert("Tam ad uzunluğu 4 ile 100 arasındda olmalıdır.");
+            return false;
+        }
+
         client.mutate({
             mutation: gql`
                 mutation signup(
@@ -59,12 +109,22 @@ const Signup = ({
             variables: {
                 fullName: fullName,
                 userName: userName,
-                password: password
+                password: md5(password)
             }
         }).then(e => {
-            console.log(e);
+            const response = e.data.signup;
+            if(response.code === 200) {
+                alert(response.message);
+                history.push("/dashboard/signin");
+            } else {
+                alert(response.message);
+            }
         }).catch(e => {
-            console.log(e.message);
+            if(e.message) {
+                alert(e.message);
+            } else {
+                alert(e);
+            }
         });
     };
 
@@ -150,6 +210,8 @@ const Signup = ({
                 <TextInput
                     placeholder="Re-Password"
                     type="password"
+                    value={rePassword}
+                    onChange={e => setRePassword(e)}
                     style={{
                         marginBottom: spaces.content * 1.5
                     }}
